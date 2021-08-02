0968639f838b653789aded0c54e2e3489ec316cd
Willy Jacquet <willy.jacquet@outlook.com>
2021-08-02 13:55:24 +0200

    Fixed normals and vertex colors.

--- src/shader/rock/phong_monochrome.fs
+++ src/shader/rock/phong_monochrome.fs
@@ -1,17 +1,22 @@
 #version 460 core
 
-uniform vec3 color_rgb = vec3(1.f, 1.f, 1.f);
+// uniform vec3 color_rgb = vec3(1.f, 1.f, 1.f);
 
+in vec3 vertex_color_rgb;
 in vec3 vertex_normal3;
 in vec3 vertex_texcoords3;
 
 out vec3 color;
 
 void main() {
+    vec3 normal3 = normalize(vertex_normal3);
+    vec3 texcoords3 = normalize(vertex_texcoords3);
+
     vec3 light_dir = vec3(0.f, 0.f, 1.f);
 
-    float lambertian = max(dot(vertex_normal3, -light_dir), 0.f);
-    vec3 diffuse = (lambertian / 2.f + 0.5f) * color_rgb;
+    float lambertian = abs(dot(normal3, -light_dir));
+    vec3 diffuse = (lambertian / 2.f + 0.5f) * vec3(1.f);
 
-    color = vertex_texcoords3 / 2.f + 0.5f;
+    color = diffuse;
+    // color = mix(vec3(1.f, 0.f, 0.f), vec3(1.f), length(normal3));
 }
--- src/shader/rock/phong_monochrome.vs
+++ src/shader/rock/phong_monochrome.vs
@@ -3,14 +3,17 @@
 uniform mat4 mvp;
 uniform mat4 normal_transform;
 
-layout(location = 0) in vec3 normal3;
-layout(location = 1) in vec3 position3;
-layout(location = 2) in vec3 texcoords3;
+layout(location = 0) in vec3 color_rgb;
+layout(location = 1) in vec3 normal3;
+layout(location = 2) in vec3 position3;
+layout(location = 3) in vec3 texcoords3;
 
+out vec3 vertex_color_rgb;
 out vec3 vertex_normal3;
 out vec3 vertex_texcoords3;
 
 void main() {
+    vertex_color_rgb = color_rgb;
     vertex_normal3 = normalize((normal_transform * vec4(normal3, 0.f)).xyz);
     vertex_texcoords3 = texcoords3;
     
