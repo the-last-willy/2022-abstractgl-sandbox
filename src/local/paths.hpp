29ed3edd0c821cf16de77b5728c33c11fe2afbd7
Willy Jacquet <willy.jacquet@outlook.com>
2021-08-24 18:09:39 +0200

    Hardcoded paths.

---
+++ src/local/paths.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+namespace local {
+
+const auto root_folder = std::string("D:/dev/project/sandbox/");
+
+const auto log_folder = root_folder + "logs/";
+const auto shader_folder = root_folder + "src/shader/";
+
+}
