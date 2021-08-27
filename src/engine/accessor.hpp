f8cb1c310b6721c4cb0dad2f6be4b34a23ea49dc
Willy Jacquet <willy.jacquet@outlook.com>
2021-08-27 17:28:16 +0200

    Min/max values.

--- src/engine/accessor.hpp
+++ src/engine/accessor.hpp
@@ -5,6 +5,7 @@
 #include <agl/all.hpp>
 
 #include <memory>
+#include <vector>
 
 namespace eng {
 
@@ -14,8 +15,11 @@ struct Accessor {
     agl::Offset<GLuint> byte_offset = agl::Offset<GLuint>(0);
     agl::Size<GLint> component_count = agl::Size<GLint>(0);
     int component_size = 0; // In bytes.
-    GLenum component_type = GL_NONE; 
+    GLenum component_type = GL_NONE;
     agl::Normalized normalized;
+
+    std::vector<float> min = {};
+    std::vector<float> max = {};
 
     // Buffer view.
 
@@ -34,6 +38,24 @@ const T& at(const Accessor& a, std::size_t i) {
     + static_cast<std::size_t>(a.byte_offset)
     + i * static_cast<std::size_t>(a.buffer_view_byte_stride);
     return reinterpret_cast<const T&>(data[offset]);
+}
+
+template<typename T>
+std::optional<T> min(const Accessor& a) {
+    if(empty(a.min)) {
+        return std::nullopt;
+    } else {
+        return *reinterpret_cast<const T*>(data(a.min));
+    }
+}
+
+template<typename T>
+std::optional<T> max(const Accessor& a) {
+    if(empty(a.max)) {
+        return std::nullopt;
+    } else {
+        return *reinterpret_cast<const T*>(data(a.max));
+    }
 }
 
 }
