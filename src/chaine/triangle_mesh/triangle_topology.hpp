2265d9f1ae9e427602f401432d54abb07e94380a
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-03 21:24:35 +0200

    Fixed missing template parameter.

--- src/chaine/triangle_mesh/triangle_topology.hpp
+++ src/chaine/triangle_mesh/triangle_topology.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "triangle_mesh_vertex_index.hpp"
+#include "vertex_index.hpp"
 
 #include <array>
 
 namespace chaine::triangle_mesh {
 
 struct TriangleTopology {
-    std::array<VertexIndex> vertices = {};
+    std::array<VertexIndex, 3> vertices = {};
 };
 
 }
