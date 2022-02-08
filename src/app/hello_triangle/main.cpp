b274fe1e0d422ff7e78df4cb72f8a1e9930a8092
Willy Jacquet <willy.jacquet@outlook.com>
2022-02-08 11:44:51 +0100

    Added missing window hint for opengl debug context

--- src/app/hello_triangle/main.cpp
+++ src/app/hello_triangle/main.cpp
@@ -34,6 +34,7 @@ void throwing_main() {
     const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(1280, 720, "OpenGL", NULL, NULL);
